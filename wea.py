d9ae882709c9d7b31455915ec0fc5f4880b86e98
Vaishnavi <153257933+vaish-navi1@users.noreply.github.com>
2024-08-04 11:40:27 +0530

    Add files via upload

---
+++ wea.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+dataset = pd.read_csv('weatherAUS.csv')
+X = dataset.iloc[:,[1,2,3,4,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]].values
+Y = dataset.iloc[:,-1].values
+print(X)
+print(Y)
+Y = Y.reshape(-1,1)
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan,strategy='most_frequent')
+X = imputer.fit_transform(X)
+Y = imputer.fit_transform(Y)
+print(X)
+print(Y)
+from sklearn.preprocessing import LabelEncoder
+le1 = LabelEncoder()
+X[:,0] = le1.fit_transform(X[:,0])
+le2 = LabelEncoder()
+X[:,4] = le2.fit_transform(X[:,4])
+le3 = LabelEncoder()
+X[:,6] = le3.fit_transform(X[:,6])
+le4 = LabelEncoder()
+X[:,7] = le4.fit_transform(X[:,7])
+le5 = LabelEncoder()
+X[:,-1] = le5.fit_transform(X[:,-1])
+le6 = LabelEncoder()
+Y[:,-1] = le6.fit_transform(Y[:,-1])
+print(X)
+print(Y)
+Y = np.array(Y,dtype=float)
+print(Y)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X = sc.fit_transform(X)
+print(X)
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.2,random_state=0)
+print(X_train)
+print(Y_train)
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=100,random_state=0)
+classifier.fit(X_train,Y_train)
+classifier.score(X_train,Y_train )
+     
+y_pred = classifier.predict(X_test)
+print(Y_pred)
+
+y_pred = le6.inverse_transform(y_pred)
+Y_test = le6.inverse_transform(y_test)
+print(y_pred)
+print(Y_test)
+
+y_pred = y_pred.reshape(-1,1)
+Y_test = Y_test.reshape(-1,1)
+     
+
+df = np.concatenate((Y_test,y_pred),axis=1)
+dataframe = pd.DataFrame(df,columns=['Rain on Tommorrow','Predition of Rain'])
+     
+
+print(dataframe)
+from sklearn.metrics import accuracy_score
+print(accuracy_score(Y_test,y_pred))
